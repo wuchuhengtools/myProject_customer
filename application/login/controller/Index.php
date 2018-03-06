d7f4316972fd591a483ace9f5afdb65f35723e12
wuChuHeng <tnmrlj@163.com>
2018-03-06 11:46:42 +0800

    登录开发中

--- application/login/controller/Index.php
+++ application/login/controller/Index.php
@@ -1,10 +1,18 @@
 <?php
 namespace app\login\controller;
 use think\Controller;
+use think\facade\Session;
 class Index extends Controller
 {
+    /**
+     *登录页面
+     *
+     */
     public function index()
     {
+        
         return $this->fetch('login'); 
     }
+
+    
 }
